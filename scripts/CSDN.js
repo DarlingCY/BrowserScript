63ea18e8d14ffbd7d2b36fc9fe0d73b3bf80204d
ChenYue <3309168033@qq.com>
2023-03-07 10:03:09 +0800

    update--CSDN

--- scripts/CSDN.js
+++ scripts/CSDN.js
@@ -3,7 +3,7 @@
 // @namespace   https://blog.csdn.net/*
 // @match       https://blog.csdn.net/*
 // @grant       none
-// @version     0.1
+// @version     0.4
 // @author      13号寄信人
 // @description 净化CSDN
 // @license     MIT
@@ -11,10 +11,19 @@
 
 (function () {
     'use strict';
+    let all = []
     //博文左边侧边栏
-    let blogAsideArr = document.getElementsByClassName("blog_container_aside");
-    for (let i = 0; i < blogAsideArr.length; i++) {
-        let self = blogAsideArr[i]
+    all.push(...document.getElementsByClassName("blog_container_aside"))
+    //博文分类专栏
+    all.push(document.getElementById("rightAside"))
+    //博文底部右侧操作栏
+    all.push(...document.getElementsByClassName("csdn-side-toolbar"))
+    for (let i = 0; i < all.length; i++) {
+        let self = all[i]
         self.parentElement.removeChild(self)
     }
+    //博文内容
+    let contentMainBox = document.getElementById("mainBox")
+    contentMainBox.style.display = "flex"
+    contentMainBox.style.justifyContent = "center"
 })();
