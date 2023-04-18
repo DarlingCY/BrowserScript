9018462058a773a6e1bc283f670cd10e04f1bf5a
ChenYue <3309168033@qq.com>
2023-04-18 09:54:10 +0800

    update--CSDN

--- scripts/CSDN.js
+++ scripts/CSDN.js
@@ -3,7 +3,7 @@
 // @namespace   https://blog.csdn.net/*
 // @match       https://blog.csdn.net/*
 // @grant       none
-// @version     0.5
+// @version     0.6
 // @author      13号寄信人
 // @description 净化CSDN
 // @license     MIT
@@ -11,8 +11,16 @@
 
 (function () {
     'use strict';
-    const css =
-        `
+
+    appendStyle(buildCss())
+
+    function buildCss() {
+        if (browserDetection() !== "PC") {
+            //------------------------移动端------------------------
+            return ``
+        } else {
+            //------------------------桌面端------------------------
+            return `
         .blog_container_aside {
             display: none !important;
         }
@@ -27,12 +35,37 @@
             justfy-content: center !important;
         }
         `
+        }
+    }
 
-    //------------------------移动端------------------------
-    let mobileAll = []
-    mobileAll.push(...document.getElementsByClassName('guide-box'))
-    for (let i = 0; i < mobileAll.length; i++) {
-        let self = mobileAll[i]
-        self.parentElement.removeChild(self)
+    function appendStyle(css) {
+        let styleNode = document.createElement("style");
+        styleNode.appendChild(document.createTextNode(css));
+        (document.querySelector("head") || document.documentElement).appendChild(styleNode);
+    }
+
+    function browserDetection() {
+        const userAgent = window.navigator.userAgent.toLowerCase();
+        let browser = null;
+        if (userAgent.match(/ipad/i)) {
+            browser = 'ipad';
+        } else if (userAgent.match(/iphone os/i)) {
+            browser = 'iphone';
+        } else if (userAgent.match(/midp/i)) {
+            browser = 'midp'
+        } else if (userAgent.match(/rv:1.2.3.4/i)) {
+            browser = 'rv:1.2.3.4';
+        } else if (userAgent.match(/ucweb/i)) {
+            browser = 'ucweb';
+        } else if (userAgent.match(/android/i)) {
+            browser = 'android';
+        } else if (userAgent.match(/windows ce/i)) {
+            browser = 'windowsCe';
+        } else if (userAgent.match(/windows mobile/i)) {
+            browser = 'windowsMobile';
+        } else {
+            browser = 'PC'
+        }
+        return browser;
     }
 })();
